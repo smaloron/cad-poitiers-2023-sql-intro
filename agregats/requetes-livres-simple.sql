99d6873a5173e4f012ec2ff9a89fe9b9ad3b0ff4
seb <contact@smaloron.eu>
2023-01-31 11:25:59 +0100

    agrégations

---
+++ agregats/requetes-livres-simple.sql
@@ -0,0 +1,29 @@
+-- Moyenne des prix des livres dont le genre est Informatique
+SELECT AVG(prix) as moyenne FROM livres_simples
+WHERE genre = 'Informatique';
+
+SELECT MAX(prix) as prix_max FROM livres_simples 
+WHERE editeur='Grasset';
+
+
+
+SELECT COUNT(*) as nb FROM livres_simples WHERE genre LIKE 'roman%';
+
+SELECT editeur, auteur, count(*) as nb FROM livres_simples 
+GROUP BY editeur, auteur HAVING nb >1 ORDER BY nb DESC;
+
+-- nombre de livres et moyenne du prix par genre
+SELECT genre, COUNT(*) as nb, AVG(prix) as prix_moyen 
+FROM livres_simples
+GROUP BY genre;
+
+-- nombre d'auteurs distincts par éditeurs COUNT(DISTINCT auteur)
+SELECT editeur, COUNT(DISTINCT auteur) as nb_auteurs
+FROM livres_simples
+GROUP BY editeur;
+
+-- La liste des auteurs qui travaillent avec plus d'un éditeur
+SELECT auteur FROM livres_simples
+GROUP BY auteur 
+HAVING COUNT(DISTINCT editeur) > 1;
+
