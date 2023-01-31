d9afda189a2b1b0d48ac3ffc7988d610790a4b3e
seb <contact@smaloron.eu>
2023-01-31 14:59:01 +0100

    concat with separator

--- multi-table/auto-jointure.sql
+++ multi-table/auto-jointure.sql
@@ -14,6 +14,6 @@ UPDATE personnes set id_boss = 4 WHERE id IN (1,2,3);
 UPDATE personnes set id_boss = 5 WHERE id IN (4,6);
 
 SELECT 
-p.nom, p.prenom, boss.nom as nom_boss, boss.prenom as prenom_boss FROM personnes as p 
+CONCAT_WS(' ',p.nom, p.prenom) as employe, CONCAT_WS(' ',boss.nom, boss.prenom) as boss FROM personnes as p 
 LEFT JOIN personnes as boss
 ON p.id_boss = boss.id;
