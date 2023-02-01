61113a6cbdc32a1a45636ff4a744e36d2bfae7de
seb <contact@smaloron.eu>
2023-02-01 09:46:00 +0100

    pizzas : creation des tables

--- pizzas.sql
+++ pizzas.sql
@@ -8,14 +8,48 @@ Modéliser une application de création pizza
 Prix de la pizza
 - la taille indique un prix de base
 - on fait ensuite la somme du prix des ingrédients
-- On applique à la somme du prix des ingrédients un coefficient multiplicateur qui dépent de la taille
+- On applique à la somme du prix des ingrédients un coefficient multiplicateur qui dépend de la taille
 par exemple petit : 1, moyenne : 1.5, grande : 2
 
 Faire une requête qui affiche toutes les pizzas dans toutes les tailles possibles avec leur tarif
 
-Tables
+Tables :
+----------
 pizzas
 ingredients_pizza
 recettes_pizza
 tailles_pizza
 */
+
+CREATE TABLE IF NOT EXISTS pizzas (
+    id SMALLINT UNSIGNED AUTO_INCREMENT,
+    nom VARCHAR(50) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS ingredients_pizza (
+    id SMALLINT UNSIGNED AUTO_INCREMENT,
+    nom VARCHAR(50) NOT NULL,
+    prix DECIMAL(4,2) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS recettes_pizza (
+    id_pizza SMALLINT UNSIGNED,
+    id_ingredient SMALLINT UNSIGNED,
+    PRIMARY KEY(id_pizza, id_ingredient),
+    CONSTRAINT recette_to_pizza
+        FOREIGN KEY (id_pizza)
+        REFERENCES pizzas(id),
+    CONSTRAINT recette_to_ingredient
+        FOREIGN KEY (id_ingredient)
+        REFERENCES ingredients_pizza(id)
+);
+
+CREATE TABLE IF NOT EXISTS tailles_pizza (
+    id TINYINT UNSIGNED AUTO_INCREMENT,
+    nom VARCHAR(30) NOT NULL,
+    prix DECIMAL(4,2) NOT NULL,
+    coeficient DECIMAL(2,1),
+    PRIMARY KEY(id)
+);
