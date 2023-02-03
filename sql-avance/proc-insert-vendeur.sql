3da35bbf7e41ad712f35d343dcfd127f9aed1ae4
seb <contact@smaloron.eu>
2023-02-03 17:10:35 +0100

    procedure stockeé gestion des erreurs

--- sql-avance/proc-insert-vendeur.sql
+++ sql-avance/proc-insert-vendeur.sql
@@ -9,10 +9,17 @@ CREATE PROCEDURE new_saleman(
     p_chef INT
 )
 BEGIN
-    INSERT INTO vendeurs (nom, cp, chef) VALUES (p_nom, p_cp, p_chef);
+	IF TRIM(p_nom) != "" AND TRIM(p_cp) != "" THEN
+		INSERT INTO vendeurs 
+			(nom, cp, chef) 
+			VALUES (p_nom, p_cp, p_chef);
+	ELSE
+		SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Arguments invalides';
+	END IF;
 END //
 
 DELIMITER ;
 
 
-CALL new_saleman('toto', '59000', 1);
+CALL new_saleman('toto', '', 1);
